a15397a71a306e05284c667109a42c4ae3d11d3e
b20724964 m. aşkın pınar <b20724964@cs.hacettepe.edu.tr>
2022-09-13 16:12:51 +0300

    gereksiz satırlar silindi

--- src/main/java/yte/intern/proje/academician/entity/Academician.java
+++ src/main/java/yte/intern/proje/academician/entity/Academician.java
@@ -4,12 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import yte.intern.proje.common.entity.BaseEntity;
 import yte.intern.proje.lecture.entity.Lecture;
-import yte.intern.proje.student.entity.Student;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity(name = "academicians")
@@ -25,14 +22,6 @@ public class Academician extends BaseEntity {
         this.surname = surname;
         this.email = email;
     }
-
-    //@ManyToMany(cascade = CascadeType.PERSIST)
-    //@JoinTable(
-    //        name = "TAG_VEHICLE",
-    //        joinColumns = @JoinColumn(name = "TAG_ID"),
-    //        inverseJoinColumns = @JoinColumn(name = "VEHICLE_ID")
-    //)
-    //protected List<Student> vehicles = new ArrayList<>();
 
     @ManyToMany(mappedBy = "academicians")
     protected Set<Lecture> lectures = new HashSet<>();
